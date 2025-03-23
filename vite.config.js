365471a81db63d52680e7fb9f6eb05a5268ba0e2
chia313339 <chia313339@gmail.com>
2025-03-23 20:07:49 +0800

    20250323

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // 讓伺服器綁定到所有可用的網卡，包括本地和局域網IP
+  },
+  optimizeDeps: {
+    include: ['bootstrap']
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
